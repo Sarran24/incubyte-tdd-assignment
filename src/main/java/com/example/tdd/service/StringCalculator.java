c332ed0f2af283adbd93a094c4921c5952f51e72
sarran <sarransher@gmail.com>
2025-10-28 23:18:55 +0530

    green: support comma and newline separated numbers

--- src/main/java/com/example/tdd/service/StringCalculator.java
+++ src/main/java/com/example/tdd/service/StringCalculator.java
@@ -1,9 +1,15 @@
 package com.example.tdd.service;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     public int add(String input) {
         if (input == null || input.isEmpty()) return 0;
-        return Integer.parseInt(input) ;
+        if (input.contains(",") || input.contains("\n")) {
+            String[] parts = input.split("[,\n" +
+                    "]");
+            return Arrays.stream(parts).mapToInt(Integer::parseInt).sum();
+        }
+        return Integer.parseInt(input);
     }
-
 }
